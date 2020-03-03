5f68f0c8dfabe63886f2d9f9aa87de22b021f369
fnajer <lexactr@gmail.com>
2020-03-04 00:39:17 +0300

    create Promo and Peer modules

---
+++ src/modules/Peer/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
+
+import { Container, BGContainerCSS } from 'shared/layout/Container';
+import Heading from 'shared/components/Heading';
+import Text from 'shared/components/Text';
+
+import peer from './peer.jpg';
+
+import * as themes from 'shared/constants/themes'
+import * as SCREEN_SIZES from 'shared/constants/screenSizes';
+
+const BGWrapper = styled.div`
+  @media (max-width: ${SCREEN_SIZES.beforeXLARGE}) {
+    padding-top: 407px;
+    background: url(${peer}) 65% 8% no-repeat;
+  }
+
+  @media (min-width: ${SCREEN_SIZES.XLARGE}) {
+    background: url(${peer}) 15% 27% no-repeat;
+    ${BGContainerCSS}
+  }
+`
+const ColPeer = styled.div`
+  @media (min-width: ${SCREEN_SIZES.XLARGE}) {
+    max-width: 445px;
+    margin-left: auto;
+  }
+`
+const Row = styled.div`
+  margin: 0 auto;
+`
+
+function Peer() {
+  return (
+    <BGWrapper>
+      <Container>
+        <Row>
+          <ColPeer>
+            <Heading theme={themes.DARK}>
+              <FormattedMessage id="peer.title" />
+            </Heading>
+            <Text theme={themes.DARK}>
+              <FormattedMessage id="peer.description" />
+            </Text>
+          </ColPeer>
+        </Row>
+      </Container>
+    </BGWrapper>
+  );
+}
+
+export default Peer;
---
+++ src/modules/Promo/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
+
+import Header from 'modules/Header';
+import { Container } from 'shared/layout/Container';
+import Heading from 'shared/components/Heading';
+import Text from 'shared/components/Text';
+import Button from 'shared/components/Button';
+
+import promo from './promo.jpg';
+import promoMobileMedium from './promo-mobile-medium.jpg';
+import promoMobile from './promo-mobile.jpg';
+
+import * as themes from 'shared/constants/themes'
+import * as SCREEN_SIZES from 'shared/constants/screenSizes';
+
+const BGWrapper = styled.div`
+  @media (max-width: ${SCREEN_SIZES.SMALL}) {
+    background: url(${promoMobile}) 50% 100% no-repeat;
+    background-size: cover;
+  }
+
+  @media (min-width: ${SCREEN_SIZES.SMALL}) and (max-width: ${SCREEN_SIZES.MEDIUM}) {
+    background: url(${promoMobileMedium}) 50% 70% no-repeat;
+    background-size: cover;
+  }
+
+  @media (min-width: ${SCREEN_SIZES.MEDIUM}) {
+    background: url(${promo}) 50% 100% no-repeat;
+    background-size: cover;
+  }
+`
+const PromoContainer = styled(Container)`
+  @media (max-width: ${SCREEN_SIZES.MEDIUM}) {
+    padding-top: 32px;
+    padding-bottom: 16px;
+  }
+  @media (min-width: ${SCREEN_SIZES.MEDIUM}) {
+    padding-top: 124px;
+    padding-bottom: 124px;
+  }
+`
+const ColPromo = styled.div`
+  @media (min-width: ${SCREEN_SIZES.MEDIUM}) {
+    max-width: 445px;
+  }
+`
+const LeftButton = styled(Button)`
+  margin-right: 24px;
+  @media (max-width: ${SCREEN_SIZES.MEDIUM}) {
+    margin-bottom: 16px;
+  }
+`
+
+const Row = styled.div`
+  margin: 0 auto;
+`
+
+function Promo({ lang, setLang }) {
+  return (
+    <BGWrapper>
+      <Header activeLang={lang} changeLang={setLang} />
+      <PromoContainer>
+        <Row>
+          <ColPromo>
+            <Heading theme={themes.LIGHT}>
+              <FormattedMessage id="main.title" />
+            </Heading>
+            <Text theme={themes.LIGHT}>
+              <FormattedMessage id="main.description" />
+            </Text>
+            <LeftButton theme={themes.LIGHT}>
+              <FormattedMessage id="shared.buttons.join" />
+            </LeftButton>
+            <Button theme={themes.BLUE}>
+              <FormattedMessage id="shared.buttons.read" />
+            </Button>
+          </ColPromo>
+        </Row>
+      </PromoContainer>
+    </BGWrapper>
+  );
+}
+
+export default Promo;
