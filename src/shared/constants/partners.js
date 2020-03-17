3cbff42203997e0a2d101643d5b4b9dcc3bffaed
fnajer <lexactr@gmail.com>
2020-03-17 13:54:36 +0300

    add information constants

--- src/shared/constants/partners.js
+++ src/shared/constants/partners.js
@@ -14,6 +14,25 @@ import FundImgMobile from 'shared/images/partners/fund-mobile.jpg';
 import CoredoImgMobile from 'shared/images/partners/coredo-mobile.jpg';
 import CryptoglobeImgMobile from 'shared/images/partners/cryptoglobe-mobile.jpg';
 
+import BitBazaImg from 'shared/images/information/bitBaza.png';
+import BitcoinGardenImg from 'shared/images/information/bitcoin-Garden-Forum.png';
+import BitkoinComImg from 'shared/images/information/bitkoin-com.png';
+import BitkoinTalkImg from 'shared/images/information/bitkointalk.png';
+import BitmmgpImg from 'shared/images/information/bitmmgp.png';
+import BitsMediaComImg from 'shared/images/information/bits-media.png';
+import BitsMediaImg from 'shared/images/information/bits-media.png';
+import CryptoCreditCardImg from 'shared/images/information/crypto-credit-card.png';
+import CryptoProComImg from 'shared/images/information/crypto-pro.png';
+import CryptocurrebcyImg from 'shared/images/information/cryptocurrebcy.png';
+import CryptoheresyImg from 'shared/images/information/cryptoheresy.png';
+import EquitiesImg from 'shared/images/information/equities.png';
+import ForbesImg from 'shared/images/information/forbes.png';
+import LogoImg from 'shared/images/information/logo.png';
+import MarketImg from 'shared/images/information/market.png';
+import NewsAustraliaTodayImg from 'shared/images/information/news_australia_today.png';
+import UzmanImg from 'shared/images/information/uzman.png';
+import VcImg from 'shared/images/information/vc_2.png';
+
 export default [
   {
     name: <FormattedMessage id="partners.title" />,
@@ -85,62 +104,110 @@ export default [
     list: [
       [
         {
-          name: 'Wallbtc1',
-          image: WallbtcImg,
-          imageMobile: WallbtcImgMobile,
+          name: 'BitBaza1',
+          image: BitBazaImg,
+          imageMobile: BitBazaImg,
         },
         {
-          name: 'Bitcoin1',
-          image: BitcoinImg,
-          imageMobile: BitcoinImgMobile,
+          name: 'BitcoinGarden1',
+          image: BitcoinGardenImg,
+          imageMobile: BitcoinGardenImg,
         },
       ],
       [
         {
-          name: 'Fund1',
-          image: FundImg,
-          imageMobile: FundImgMobile,
+          name: 'BitkoinCom1',
+          image: BitkoinComImg,
+          imageMobile: BitkoinComImg,
         },
         {
-          name: 'Cryptoglobe1',
-          image: CryptoglobeImg,
-          imageMobile: CryptoglobeImgMobile,
+          name: 'BitkoinTalk1',
+          image: BitkoinTalkImg,
+          imageMobile: BitkoinTalkImg,
         }
       ],
       [
         {
-          name: 'Coredo1',
-          image: CoredoImg,
-          imageMobile: CoredoImgMobile,
+          name: 'Bitmmgp1',
+          image: BitmmgpImg,
+          imageMobile: BitmmgpImg,
         },
         {
-          name: 'Whalesburg1',
-          image: WhalesburgImg,
-          imageMobile: WhalesburgImgMobile,
-        },
-      ],
-      [
-        {
-          name: 'Coredo2',
-          image: CoredoImg,
-          imageMobile: CoredoImgMobile,
-        },
-        {
-          name: 'Whalesburg2',
-          image: WhalesburgImg,
-          imageMobile: WhalesburgImgMobile,
+          name: 'BitsMediaCom1',
+          image: BitsMediaComImg,
+          imageMobile: BitsMediaComImg,
         },
       ],
       [
         {
-          name: 'Wallbtc2',
-          image: WallbtcImg,
-          imageMobile: WallbtcImgMobile,
+          name: 'BitsMedia1',
+          image: BitsMediaImg,
+          imageMobile: BitsMediaImg,
         },
         {
-          name: 'Bitcoin2',
-          image: BitcoinImg,
-          imageMobile: BitcoinImgMobile,
+          name: 'CryptoCreditCard1',
+          image: CryptoCreditCardImg,
+          imageMobile: CryptoCreditCardImg,
+        },
+      ],
+      [
+        {
+          name: 'CryptoProCom1',
+          image: CryptoProComImg,
+          imageMobile: CryptoProComImg,
+        },
+        {
+          name: 'Cryptocurrebcy1',
+          image: CryptocurrebcyImg,
+          imageMobile: CryptocurrebcyImg,
+        },
+      ],
+      [
+        {
+          name: 'Cryptoheresy1',
+          image: CryptoheresyImg,
+          imageMobile: CryptoheresyImg,
+        },
+        {
+          name: 'Equities1',
+          image: EquitiesImg,
+          imageMobile: EquitiesImg,
+        },
+      ],
+      [
+        {
+          name: 'Forbes1',
+          image: ForbesImg,
+          imageMobile: ForbesImg,
+        },
+        {
+          name: 'Logo1',
+          image: LogoImg,
+          imageMobile: LogoImg,
+        },
+      ],
+      [
+        {
+          name: 'Market1',
+          image: MarketImg,
+          imageMobile: MarketImg,
+        },
+        {
+          name: 'NewsAustraliaToday1',
+          image: NewsAustraliaTodayImg,
+          imageMobile: NewsAustraliaTodayImg,
+        },
+      ],
+      [
+        {
+          name: 'Uzman1',
+          image: UzmanImg,
+          imageMobile: UzmanImg,
+        },
+        {
+          name: 'Vc1',
+          image: VcImg,
+          imageMobile: VcImg,
         },
       ],
     ]
