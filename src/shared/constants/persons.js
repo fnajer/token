db761d1da31a3f958a9a624b5c07a5baa4cb9842
fnajer <lexactr@gmail.com>
2020-03-17 13:55:12 +0300

    add partners constants

--- src/shared/constants/persons.js
+++ src/shared/constants/persons.js
@@ -9,8 +9,19 @@ import AnatolyPhotoMobile from 'shared/images/team/team-mobile-1.jpg';
 import MargaritaPhotoMobile from 'shared/images/team/team-mobile-2.jpg';
 import DmitryPhotoMobile from 'shared/images/team/team-mobile-3.jpg';
 import NikitaPhotoMobile from 'shared/images/team/team-mobile-4.jpg';
+import PetruPhoto from 'shared/images/team/team-5.png';
+import LeonovaPhoto from 'shared/images/team/team-6.jpg';
+import RobertPhoto from 'shared/images/team/team-7.jpg';
 
 import MariaPhoto from 'shared/images/advisors/member-1.jpg';
+import SurovaPhoto from 'shared/images/advisors/member-2.png';
+import EvgenyPhoto from 'shared/images/advisors/member-3.png';
+import ArtyomPhoto from 'shared/images/advisors/member-4.jpg';
+import EfremovPhoto from 'shared/images/advisors/member-5.png';
+import GaryPhoto from 'shared/images/advisors/member-6.png';
+import DavidPhoto from 'shared/images/advisors/member-7.png';
+import VladimirPhoto from 'shared/images/advisors/member-8.png';
+import SimonPhoto from 'shared/images/advisors/member-9.jpg';
 
 export default [
   {
@@ -47,7 +58,32 @@ export default [
         name: <FormattedMessage id="team.member4.name" />,
         description: <FormattedMessage id="team.member4.description" />,
         instagram: 'https://instagram.com',
-      }
+      },
+      {
+        image: PetruPhoto,
+        imageMobile: PetruPhoto,
+        position: <FormattedMessage id="team.member5.specialization" />,
+        name: <FormattedMessage id="team.member5.name" />,
+        description: <FormattedMessage id="team.member5.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: LeonovaPhoto,
+        imageMobile: LeonovaPhoto,
+        position: <FormattedMessage id="team.member6.specialization" />,
+        name: <FormattedMessage id="team.member6.name" />,
+        description: <FormattedMessage id="team.member6.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: RobertPhoto,
+        imageMobile: RobertPhoto,
+        position: <FormattedMessage id="team.member7.specialization" />,
+        name: <FormattedMessage id="team.member7.name" />,
+        description: <FormattedMessage id="team.member7.description" />,
+        instagram: 'https://instagram.com',
+      },
+      
     ]
   },
   {
@@ -59,6 +95,70 @@ export default [
         position: <FormattedMessage id="advisors.member1.specialization" />,
         name: <FormattedMessage id="advisors.member1.name" />,
         description: <FormattedMessage id="advisors.member1.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: SurovaPhoto,
+        imageMobile: SurovaPhoto,
+        position: <FormattedMessage id="advisors.member2.specialization" />,
+        name: <FormattedMessage id="advisors.member2.name" />,
+        description: <FormattedMessage id="advisors.member2.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: EvgenyPhoto,
+        imageMobile: EvgenyPhoto,
+        position: <FormattedMessage id="advisors.member3.specialization" />,
+        name: <FormattedMessage id="advisors.member3.name" />,
+        description: <FormattedMessage id="advisors.member3.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: ArtyomPhoto,
+        imageMobile: ArtyomPhoto,
+        position: <FormattedMessage id="advisors.member4.specialization" />,
+        name: <FormattedMessage id="advisors.member4.name" />,
+        description: <FormattedMessage id="advisors.member4.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: EfremovPhoto,
+        imageMobile: EfremovPhoto,
+        position: <FormattedMessage id="advisors.member5.specialization" />,
+        name: <FormattedMessage id="advisors.member5.name" />,
+        description: <FormattedMessage id="advisors.member5.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: GaryPhoto,
+        imageMobile: GaryPhoto,
+        position: <FormattedMessage id="advisors.member6.specialization" />,
+        name: <FormattedMessage id="advisors.member6.name" />,
+        description: <FormattedMessage id="advisors.member6.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: DavidPhoto,
+        imageMobile: DavidPhoto,
+        position: <FormattedMessage id="advisors.member7.specialization" />,
+        name: <FormattedMessage id="advisors.member7.name" />,
+        description: <FormattedMessage id="advisors.member7.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: VladimirPhoto,
+        imageMobile: VladimirPhoto,
+        position: <FormattedMessage id="advisors.member8.specialization" />,
+        name: <FormattedMessage id="advisors.member8.name" />,
+        description: <FormattedMessage id="advisors.member8.description" />,
+        instagram: 'https://instagram.com',
+      },
+      {
+        image: SimonPhoto,
+        imageMobile: SimonPhoto,
+        position: <FormattedMessage id="advisors.member9.specialization" />,
+        name: <FormattedMessage id="advisors.member9.name" />,
+        description: <FormattedMessage id="advisors.member9.description" />,
         instagram: 'https://instagram.com',
       },
     ]
